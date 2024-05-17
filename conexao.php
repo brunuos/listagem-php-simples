90338b97f44d6013f73efadbad1b2182469cdcd7
Bruno <bsaragao1990@gmail.com>
2024-05-17 15:30:47 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,16 +1,12 @@
 <?php
-
 $host ='localhost';
 $user ='root';
 $password ='';
 $db ='bdagenda';
 
-// Create a new mysqli object
 $mysqli = new mysqli($host, $user, $password, $db);
 
-// Check for errors
 if ($mysqli->connect_error) {
     die('Connection error: ' . $mysqli->connect_error);
 }
-
 ?>
